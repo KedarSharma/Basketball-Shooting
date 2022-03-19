8a4299cebba5fa0701b08c2e5958d4071e4fb903
KedarSharma <66473179+KedarSharma@users.noreply.github.com>
2022-03-19 16:57:18 +0530

    Update basketball.js

--- basketball.js
+++ basketball.js
@@ -10,15 +10,15 @@ class Basketball {
 		this.radius = 25;
 		this.body = Matter.Bodies.circle(x,y,this.radius,options);
 		this.image = loadImage("images/Basketball.png");
-		this.constraint = Matter.Constraint.create({
+		/*this.constraint = Matter.Constraint.create({
 			length : 10,
 			stiffness : 5,
 			bodyA : this.body,
 			pointB : {x : 200,y : 200}
-		});
+		});*/
 		Matter.Body.setAngle(this.body,angle);
 		World.add(world,this.body);
-		World.add(world,this.constraint)
+		//World.add(world,this.constraint)
 	}
 
 	display(){
